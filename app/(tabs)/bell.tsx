c0f066f4f89b78b0d3bb920a5c5cb78400b49580
GNatroshvili <gigi.natroshvili@gmail.com>
2025-06-13 12:23:43 +0400

    Finish notifications page

--- app/(tabs)/bell.tsx
+++ app/(tabs)/bell.tsx
@@ -1,18 +1,33 @@
 import Notifications from "@/components/Notifications";
 import SearchInput from "@/components/SearchInput";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function NotificationsScreen() {
   return (
     <View style={styles.container}>
-      <SearchInput />
-      <View style={styles.titleWrapper}>
-        <Text style={styles.title}>
-          You can check your notifications here.
-        </Text>
+      <View style={styles.inputWrapper}>
+        <SearchInput />
       </View>
-      <Notifications/>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>You can check your notifications here.</Text>
+      </View>
+      <ScrollView
+        contentContainerStyle={styles.notificationsWrapper}
+        showsVerticalScrollIndicator={false}
+      >
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+        <Notifications />
+      </ScrollView>
     </View>
   );
 }
@@ -21,17 +36,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingHorizontal: 16,
-    paddingTop: 24,
+    paddingHorizontal: 30,
+    paddingTop: 84,
+  },
+  inputWrapper: {
+    alignItems: "center",
   },
   titleWrapper: {
     marginTop: 40,
-    maxWidth: 224
+    maxWidth: 224,
+    // marginBottom: 40,
   },
   title: {
     fontSize: 24,
     fontWeight: "700",
     fontFamily: "Montserrat",
     color: "#3A3A3A",
+  },
+  notificationsWrapper: {
+    gap: 20,
+    paddingTop: 40,
   },
 });
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,48 +1,51 @@
-import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
-import { StyleSheet, Text, TextInput, TextInputProps, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
 
 const SearchInput: React.FC<TextInputProps> = (props) => {
-    return (
-        <View style={styles.container}>
-            <Ionicons name="search-outline" size={20} color="#1A5CFF" style={styles.icon} />
-            <TextInput
-                style={styles.input}
-                placeholder="Search"
-                placeholderTextColor="#1A5CFF"
-                {...props}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Ionicons
+        name="search-outline"
+        size={20}
+        color="#1A5CFF"
+        style={styles.icon}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Search"
+        placeholderTextColor="#1A5CFF"
+        {...props}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width: 320,
-        height: 45,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#F5F6FA',
-        borderRadius: 12,
-        paddingHorizontal: 12,
-    },
-    icon: {
-        marginRight: 8,
-    },
-    input: {
-        flex: 1,
-        fontSize: 16,
-        color: '#1A5CFF',
-    },
-    titleWrapper: {
-
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        fontFamily: "Montserrat",
-        color: "#3A3A3A",
-    }
+  container: {
+    width: "100%",
+    height: 45,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F5F6FA",
+    borderRadius: 12,
+    paddingHorizontal: 12,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: "#1A5CFF",
+  },
+  titleWrapper: {},
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    fontFamily: "Montserrat",
+    color: "#3A3A3A",
+  },
 });
 
 export default SearchInput;
